b5147c21446362c828a538f7d2c0b5ec59e8930e
woojw <woojw@student.unimelb.edu.au>
2021-04-19 02:08:40 +1000

    Added definition for menu and order schema

--- models/menu.js
+++ models/menu.js
@@ -1,23 +1,50 @@
 // temporary locally stored menu
-module.exports = [
-    {
-        "id":"1",
-        "name":"Big Cake",
-        "price":"35.00",
+// module.exports = [
+//     {
+//         "id":"1",
+//         "name":"Big Cake",
+//         "price":"35.00",
+//     },
+//     {
+//         "id":"2",
+//         "name":"Cappuccino",
+//         "price":"5.00",
+//     },
+//     {
+//         "id":"3",
+//         "name":"Fancy Biscuit",
+//         "price":"3.00",
+//     },
+//     {
+//         "id":"4",
+//         "name":"Flat White",
+//         "last_pricename":"4.50",
+//     }
+// ]
+
+const mongoose = require("mongoose")
+
+// define the schema for menu database
+const menuSchema = new mongoose.Schema({
+    snackName: { 
+        type: String, 
+        required: true , 
+        unique: true
     },
-    {
-        "id":"2",
-        "name":"Cappuccino",
-        "price":"5.00",
+    image: { 
+        type: String, 
+        required: true
     },
-    {
-        "id":"3",
-        "name":"Fancy Biscuit",
-        "price":"3.00",
-    },
-    {
-        "id":"4",
-        "name":"Flat White",
-        "last_pricename":"4.50",
+    price: { 
+        type: Number, 
+        required: true , 
+        min: 0
+    }, 
+    description: { 
+        type: String
     }
-]
+})
+
+// export the menu model to be used by the controllers
+const Menu = mongoose.model("Menu", menuSchema)
+module.exports = Menu
--- models/order.js
+++ models/order.js
@@ -1,16 +1,47 @@
 // temporary locally stored orders
 // module.exports = [
+//     {
+//         "orderNumber": "0",
+//         "snacks": [
+//             {
+//                 "id": "3",
+//                 "name": "Fancy Biscuit",
+//                 "price": "3.00"
+//             }
+//         ]
+//     }
 // ]
 
-module.exports = [
-    {
-        "orderNumber": "0",
-        "snacks": [
-            {
-                "id": "3",
-                "name": "Fancy Biscuit",
-                "price": "3.00"
-            }
-        ]
+const mongoose = require("mongoose")
+
+// define the schema for one line of snacks to be usedd in the order schema
+const orderLineSchema = new mongoose.Schema({
+    snackId: {
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Menu', 
+        required: true
+    }, 
+    quantity: {
+        type: Number, 
+        required: true, 
+        min: 1
     }
-]
+})
+
+// define the schema for one order in the orders database
+const orderSchema = new mongoose.Schema({
+    orderId: { 
+        type: String, 
+        required: true , 
+        unique: true
+    }, 
+    snacks: 
+        [orderLineSchema]
+    , 
+    timestamps: 
+        true
+})
+
+// export the order model to be used by the controllers
+const Order = mongoose.model("Order", orderSchema)
+module.exports = Order
