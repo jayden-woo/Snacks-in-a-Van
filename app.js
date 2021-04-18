f62f5ff6e8d4f77abdf3d3cb4b715e9661b30c83
Xinyue Zhang <zxz1@student.unimelb.edu.au>
2021-04-18 16:43:22 +1000

    app.js edit test

--- app.js
+++ app.js
@@ -2,20 +2,19 @@ const express = require('express')
 require('./models');
 
 const app = express()
-const port = process.env.PORT || 3000;
 
 // set up author routes
 const authorRouter = require('./routes/authorRouter')
 
 // GET home page
 app.get('/', (req, res) => {
-    res.send('<h1>Library System</h1>')
+    res.send('<h1>Tester</h1>')
 })
 
 // Handle author-management requests
 // the author routes are added onto the end of '/author-management'
 app.use('/author-management', authorRouter)
 
-app.listen(port, () => {
-    console.log('Example app listening at http://localhost:${port}')
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Running!")
 })
