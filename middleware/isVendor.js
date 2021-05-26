9ba84c6855f1c0d9e5345eb0921e5e3d861b5d60
jayden-woo <woojw@student.unimelb.edu.au>
2021-05-27 06:17:27 +1000

    Replaced isLoggedIn middleware with isCustomer and isVendor

---
+++ middleware/isVendor.js
@@ -0,0 +1,20 @@
+// check if a user is currently logged in as a vendor
+const isVendor = (req, res, next) => {
+    // check if the user is logged in
+    if (!req.isAuthenticated()) {
+        console.log("Not logged in")
+        req.flash("redirectMessage", "Please log in frist before proceeding.")
+        return res.status(401).redirect("/vendor/login")
+    // check if the logged in user is a vendor
+    } else if (req.user.constructor.modelName != "Vendor") {
+        console.log("Customer accessing vendor routes")
+        req.flash("redirectMessage", "Sorry, you do not have permission to access this page.")
+        // render error page or redirect to customer main page
+        return res.status(403).redirect("/customer")
+    }
+    // user is an authenticated vendor and can proceed
+    return next()
+}
+
+// export the function
+module.exports = isVendor
